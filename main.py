7df6b2bc2eeb8f0c42fb9f6f257291bd6924cbf1
chun37 <chun.jb.37@gmail.com>
2021-08-12 18:49:42 +0900

    完成

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+from discord.ext.commands import Bot, when_mentioned
+
+
+BOT_EXTENSIONS = ["ogp"]
+
+
+class MyBot(Bot):
+    def __init__(self) -> None:
+        super().__init__(command_prefix=when_mentioned)
+        for extension in BOT_EXTENSIONS:
+            self.load_extension("extensions." + extension)
+
+    async def on_ready(self) -> None:
+        print(self.user.name)
+        print(self.user.id, "\n")
+
+
+if __name__ == "__main__":
+    bot = MyBot()
+    bot.run(os.environ["DISCORD_TOKEN"])
